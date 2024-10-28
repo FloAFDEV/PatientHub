cc1be5b0b93e8e76609ca3e0fa5b38e4dec20982
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2024-10-28 15:11:37 +0100

    "Refactored LoginPage component structure and layout"

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -93,145 +93,149 @@ export default function LoginPage() {
 	};
 
 	return (
-		<BackgroundBeamsWithCollision>
-			<div
-				suppressHydrationWarning
-				className="flex-grow flex flex-col justify-between min-h-screen"
-			>
-				{/* Affichage de la modal si authentifié */}
-				{isModalOpen && (
-					<PasskeyModal open={isModalOpen} onClose={closeModal} />
-				)}
-				<div className="flex-1 flex flex-col items-center justify-center p-4">
-					{isMounted && (
-						<div className="absolute top-4 right-4 z-10">
-							<ModeToggle />
-						</div>
+		<div className="flex h-screen max-h-screen">
+			<BackgroundBeamsWithCollision className="flex-grow">
+				<div
+					suppressHydrationWarning
+					className="flex-grow flex flex-col justify-between min-h-screen"
+				>
+					{/* Affichage de la modal si authentifié */}
+					{isModalOpen && (
+						<PasskeyModal open={isModalOpen} onClose={closeModal} />
 					)}
-					<div className="mx-auto max-w-md w-full px-4 sm:px-6 lg:max-w-2xl lg:px-8 lg:py-16 mt-28">
-						<div className="text-center">
-							<h1 className="text-2xl font-bold tracking-tight sm:text-3xl md:text-4xl lg:text-5xl">
-								Votre espace dédié aux ostéopathes
-							</h1>
-							<p className="mt-4 text-base leading-7 sm:mt-6 sm:text-lg">
-								Suivez vos patients en toute simplicité.
-								Connectez-vous pour consulter vos dossiers.
+					<div className="flex-1 flex flex-col items-center justify-center p-4">
+						{isMounted && (
+							<div className="absolute top-4 right-4 z-10">
+								<ModeToggle />
+							</div>
+						)}
+						<div className="mx-auto max-w-md w-full px-4 sm:px-6 lg:max-w-2xl lg:px-8 lg:py-16 mt-28">
+							<div className="text-center">
+								<h1 className="text-2xl font-bold tracking-tight sm:text-3xl md:text-4xl lg:text-5xl">
+									Votre espace dédié aux ostéopathes
+								</h1>
+								<p className="mt-4 text-base leading-7 sm:mt-6 sm:text-lg">
+									Suivez vos patients en toute simplicité.
+									Connectez-vous pour consulter vos dossiers.
+								</p>
+							</div>
+						</div>
+						<h1 className="absolute top-4 left-4 text-lg sm:text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500">
+							PatientHub
+						</h1>
+						<div className="w-full max-w-md text-card-foreground border p-4 shadow-lg rounded-lg mt-6 mb-12 sm:mb-24">
+							<h2 className="text-lg font-bold text-center mb-10">
+								Connexion
+							</h2>
+							<form onSubmit={handleSubmit} className="space-y-4">
+								{/* Champ Email */}
+								<div>
+									<label
+										htmlFor="email"
+										className="block text-sm font-medium"
+									>
+										Email :
+									</label>
+									<input
+										id="email"
+										name="email"
+										type="email"
+										required
+										autoComplete="email"
+										placeholder="Votre email"
+										className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary text-sm"
+										value={email}
+										onChange={(e) =>
+											setEmail(e.target.value)
+										}
+										aria-describedby="email-error"
+									/>
+								</div>
+								{/* Champ Mot de Passe */}
+								<div>
+									<label
+										htmlFor="password"
+										className="block text-sm font-medium"
+									>
+										Mot de passe :
+									</label>
+									<input
+										id="password"
+										name="password"
+										type="password"
+										required
+										autoComplete="current-password"
+										placeholder="Votre mot de passe"
+										className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary text-sm"
+										value={password}
+										onChange={(e) =>
+											setPassword(e.target.value)
+										}
+										aria-describedby="password-error"
+									/>
+								</div>
+								<input
+									type="hidden"
+									name="hiddenField"
+									value={hiddenField}
+								/>
+								{/* Bouton de soumission */}
+								<button
+									type="submit"
+									className={`w-full bg-gradient-to-r from-purple-500 to-pink-500 text-white py-2 rounded-md hover:bg-gradient-to-l focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 ${
+										isLoading ? "opacity-50" : ""
+									}`}
+									disabled={isLoading}
+									aria-busy={isLoading}
+								>
+									{isLoading
+										? "Connexion en cours..."
+										: "Se connecter"}
+								</button>
+								{/* Message d'erreur */}
+								{error && (
+									<p
+										className="text-red-500 text-xs text-center"
+										role="alert"
+									>
+										{error}
+									</p>
+								)}
+								<p className="text-sm text-muted-foreground text-center">
+									Vous n&apos;avez pas accès ?{" "}
+									<a
+										href="mailto:afdevflo@gmail.com?subject=Demande%20d'accès%20à%20PatientHub&body=Bonjour%20[Nom%20complet%20ou%20société],%0A%0AJ'aimerais%20demander%20un%20acc%C3%A8s%20%C3%A0%20la%20plateforme.%0A%0AVoici%20quelques%20informations%20:%0A%0A-%20Nom%20complet%20ou%20société%20:%20[Nom%20complet%20ou%20société]%0A- %20Email%20:%20[Votre%20adresse%20e-mail]%0A- %20Raison%20de%20la%20demande%20:%20[Expliquez%20bri%C3%A8vement%20pourquoi%20vous%20souhaitez%20acc%C3%A9der%20%C3%A0%20la%20plateforme]%0A%0AJe%20vous%20remercie%20d'avance%20pour%20votre%20aide.%0A%0AMerci%20pour%20l'intérêt%20porté.%0A%0ACordialement,%0AAFDEV"
+										className="text-sky-700 underline hover:text-sky-800"
+									>
+										Contactez l&apos;administrateur
+									</a>{" "}
+									pour plus d&apos;informations.
+								</p>
+							</form>
+						</div>
+
+						<div className="hidden md:block mt-4 text-center text-muted-foreground px-4">
+							<p className="text-base leading-tight">
+								La plateforme conçue pour faciliter la gestion
+								des données médicales.
+							</p>
+							<p className="text-base leading-tight">
+								Gérez vos rendez-vous (Bientôt disponible sur la
+								plateforme).
 							</p>
 						</div>
+						<Footer />
 					</div>
-					<h1 className="absolute top-4 left-4 text-lg sm:text-xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500">
-						PatientHub
-					</h1>
-					<div className="w-full max-w-md text-card-foreground border p-4 shadow-lg rounded-lg mt-6 mb-12 sm:mb-24">
-						<h2 className="text-lg font-bold text-center mb-10">
-							Connexion
-						</h2>
-						<form onSubmit={handleSubmit} className="space-y-4">
-							{/* Champ Email */}
-							<div>
-								<label
-									htmlFor="email"
-									className="block text-sm font-medium"
-								>
-									Email :
-								</label>
-								<input
-									id="email"
-									name="email"
-									type="email"
-									required
-									autoComplete="email"
-									placeholder="Votre email"
-									className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary text-sm"
-									value={email}
-									onChange={(e) => setEmail(e.target.value)}
-									aria-describedby="email-error"
-								/>
-							</div>
-							{/* Champ Mot de Passe */}
-							<div>
-								<label
-									htmlFor="password"
-									className="block text-sm font-medium"
-								>
-									Mot de passe :
-								</label>
-								<input
-									id="password"
-									name="password"
-									type="password"
-									required
-									autoComplete="current-password"
-									placeholder="Votre mot de passe"
-									className="mt-1 block w-full px-3 py-2 border rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary text-sm"
-									value={password}
-									onChange={(e) =>
-										setPassword(e.target.value)
-									}
-									aria-describedby="password-error"
-								/>
-							</div>
-							<input
-								type="hidden"
-								name="hiddenField"
-								value={hiddenField}
-							/>
-							{/* Bouton de soumission */}
-							<button
-								type="submit"
-								className={`w-full bg-gradient-to-r from-purple-500 to-pink-500 text-white py-2 rounded-md hover:bg-gradient-to-l focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 ${
-									isLoading ? "opacity-50" : ""
-								}`}
-								disabled={isLoading}
-								aria-busy={isLoading}
-							>
-								{isLoading
-									? "Connexion en cours..."
-									: "Se connecter"}
-							</button>
-							{/* Message d'erreur */}
-							{error && (
-								<p
-									className="text-red-500 text-xs text-center"
-									role="alert"
-								>
-									{error}
-								</p>
-							)}
-							<p className="text-sm text-muted-foreground text-center">
-								Vous n&apos;avez pas accès ?{" "}
-								<a
-									href="mailto:afdevflo@gmail.com?subject=Demande%20d'accès%20à%20PatientHub&body=Bonjour%20[Nom%20complet%20ou%20société],%0A%0AJ'aimerais%20demander%20un%20acc%C3%A8s%20%C3%A0%20la%20plateforme.%0A%0AVoici%20quelques%20informations%20:%0A%0A-%20Nom%20complet%20ou%20société%20:%20[Nom%20complet%20ou%20société]%0A- %20Email%20:%20[Votre%20adresse%20e-mail]%0A- %20Raison%20de%20la%20demande%20:%20[Expliquez%20bri%C3%A8vement%20pourquoi%20vous%20souhaitez%20acc%C3%A9der%20%C3%A0%20la%20plateforme]%0A%0AJe%20vous%20remercie%20d'avance%20pour%20votre%20aide.%0A%0AMerci%20pour%20l'intérêt%20porté.%0A%0ACordialement,%0AAFDEV"
-									className="text-sky-700 underline hover:text-sky-800"
-								>
-									Contactez l&apos;administrateur
-								</a>{" "}
-								pour plus d&apos;informations.
-							</p>
-						</form>
-					</div>
-
-					<div className="hidden md:block mt-4 text-center text-muted-foreground px-4">
-						<p className="text-base leading-tight">
-							La plateforme conçue pour faciliter la gestion des
-							données médicales.
-						</p>
-						<p className="text-base leading-tight">
-							Gérez vos rendez-vous (Bientôt disponible sur la
-							plateforme).
-						</p>
-					</div>
-					<Footer />
 				</div>
-			</div>
+			</BackgroundBeamsWithCollision>
 			<Image
 				src="/assets/images/onboarding-img.png"
-				height={800}
-				width={600}
+				height={1000}
+				width={1000}
 				priority
 				alt="Image d'accueil"
-				className="side-img max-w-[50%] hidden md:block mt-0"
+				className="side-img max-w-[50%] hidden md:block"
 			/>
-		</BackgroundBeamsWithCollision>
+		</div>
 	);
 }
