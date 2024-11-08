2e1d25a3c10eda99df72f3a0d87e6cdb1ff2bb4a
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2024-11-08 12:18:52 +0100

    "Refactor RootLayout to DashboardPage, adding specific dashboard page content"

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,10 +3,14 @@
 import React from "react";
 import SidebarDashboard from "@/components/SidebarDashboard/SidebarDashboard";
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
-	return <SidebarDashboard>{children}</SidebarDashboard>;
-}
+const DashboardPage = () => {
+	return (
+		<SidebarDashboard>
+			{/* Contenu spécifique à la page du tableau de bord */}
+			<h1 className="text-2xl font-bold">Tableau de bord</h1>
+			{/* Vous pouvez ajouter d'autres composants ou éléments ici */}
+		</SidebarDashboard>
+	);
+};
+
+export default DashboardPage;
