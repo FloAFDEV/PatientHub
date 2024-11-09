832777a5b1b8fb7992f572a68a014b79d57fd404
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2024-11-09 21:33:07 +0100

    "Refactored PatientDetails component, updated layout and styling, added new sections and fields, and modified PatientList component's loading indicator"

--- components/PatientDetails/PatientDetails.jsx
+++ components/PatientDetails/PatientDetails.jsx
@@ -5,8 +5,9 @@ const PatientDetails = ({ patient, onClose }) => {
 	const [error, setError] = useState(null);
 	const [openSections, setOpenSections] = useState({
 		basicInfo: true,
-		medicalProblems: false,
-		children: false,
+		medicalHistory: false,
+		familyInfo: false,
+		practitionerInfo: false,
 	});
 
 	const formatPhoneNumber = (phone) => {
@@ -63,6 +64,21 @@ const PatientDetails = ({ patient, onClose }) => {
 		false: "Non",
 	};
 
+	const SectionToggle = ({ title, isOpen, onToggle, children }) => (
+		<div className="mb-4">
+			<button
+				className="flex justify-between items-center w-full bg-gray-700 p-2 rounded-md hover:bg-gray-600 text-gray-200"
+				onClick={onToggle}
+			>
+				<span className="font-semibold text-sm md:text-base">
+					{title}
+				</span>
+				<span>{isOpen ? "−" : "+"}</span>
+			</button>
+			{isOpen && <div className="p-4">{children}</div>}
+		</div>
+	);
+
 	if (error) {
 		return (
 			<div className="p-4 w-full mx-auto">
@@ -76,13 +92,21 @@ const PatientDetails = ({ patient, onClose }) => {
 		);
 	}
 
+	const DetailItem = ({ label, value }) => (
+		<div className="flex justify-between items-center py-2 border-b border-gray-300">
+			<span className="font-semibold text-sm">{label}:</span>
+			<span className="text-sm">{value}</span>
+		</div>
+	);
+
 	return (
-		<div className="p-4 w-full h-screen mx-auto dark:text-gray-300 overflow-y-auto">
+		<div className="p-1 w-full h-screen mx-auto dark:text-gray-300 overflow-y-auto">
+			{/* Bouton de fermeture */}
 			<button className="mb-4 text-red-500" onClick={onClose}>
 				&times; Fermer
 			</button>
 
-			{/* Patient Info Header */}
+			{/* En-tête d'informations du patient */}
 			<div className="flex flex-col md:flex-row items-center md:items-start mb-6 space-y-4 md:space-y-0 md:space-x-6">
 				<Image
 					src={
@@ -107,165 +131,221 @@ const PatientDetails = ({ patient, onClose }) => {
 				</div>
 			</div>
 
-			{/* Basic Information Section */}
-			<div className="mb-4">
-				<button
-					className="flex justify-between items-center w-full bg-gray-700 p-2 rounded-md hover:bg-gray-600 text-gray-200"
-					onClick={() => toggleSection("basicInfo")}
-				>
-					<span className="font-semibold text-sm md:text-base">
-						Informations de base
-					</span>
-					<span>{openSections.basicInfo ? "−" : "+"}</span>
-				</button>
-				{openSections.basicInfo && (
-					<div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
-						{/* Basic info fields */}
-						<p>
-							<strong>Email:</strong>{" "}
-							{patient.email || "Non renseigné"}
-						</p>
-						<p>
-							<strong>Téléphone:</strong>{" "}
-							{formatPhoneNumber(patient.phone) ||
-								"Non renseigné"}
-						</p>
-						<p>
-							<strong>Date de Naissance:</strong>{" "}
-							{patient.birthDate
+			{/* Informations de base */}
+			<SectionToggle
+				title="Informations de base"
+				isOpen={openSections.basicInfo}
+				onToggle={() => toggleSection("basicInfo")}
+			>
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+					<DetailItem
+						label="Email"
+						value={patient.email || "Non renseigné"}
+					/>
+					<DetailItem
+						label="Téléphone"
+						value={
+							formatPhoneNumber(patient.phone) || "Non renseigné"
+						}
+					/>
+					<DetailItem
+						label="Date de Naissance"
+						value={
+							patient.birthDate
 								? new Date(
 										patient.birthDate
 								  ).toLocaleDateString("fr-FR")
-								: "Non renseignée"}
-						</p>
-						<p>
-							<strong>Genre:</strong>{" "}
-							{patient.gender || "Non renseigné"}
-						</p>
-						<p>
-							<strong>Adresse:</strong>{" "}
-							{patient.address || "Non renseignée"}
-						</p>
-						<p>
-							<strong>Statut marital:</strong>{" "}
-							{maritalStatusTranslations[patient.maritalStatus] ||
-								"Non renseigné"}
-						</p>
-						<p>
-							<strong>Métier:</strong>{" "}
-							{patient.occupation || "Non renseigné"}
-						</p>
-						<p>
-							<strong>Latéralité:</strong>{" "}
-							{handednessTranslations[patient.handedness] ||
-								"Non renseignée"}
-						</p>
-						<p>
-							<strong>Méthode de contraception:</strong>{" "}
-							{contraceptionTranslations[patient.contraception] ||
-								"Non renseignée"}
-						</p>
-						<p>
-							<strong>Fumeur ?</strong>{" "}
-							{yesNoTranslations[patient.isSmoker] ||
-								"Non renseigné"}
-						</p>
-						<p>
-							<strong>Correction visuelle ?</strong>{" "}
-							{yesNoTranslations[patient.hasVisionCorrection] ||
-								"Non renseignée"}
-						</p>
-						<p>
-							<strong>Le patient est-il décédé ?</strong>{" "}
-							{yesNoTranslations[patient.isDeceased] ||
-								"Non renseigné"}
-						</p>
-						<p>
-							<strong>Activité physique:</strong>{" "}
-							{patient.physicalActivity || "Non renseignée"}
-						</p>
+								: "Non renseignée"
+						}
+					/>
+					<DetailItem
+						label="Genre"
+						value={patient.gender || "Non renseigné"}
+					/>
+					<DetailItem
+						label="Adresse"
+						value={patient.address || "Non renseignée"}
+					/>
+					<DetailItem
+						label="Statut marital"
+						value={
+							maritalStatusTranslations[patient.maritalStatus] ||
+							"Non renseigné"
+						}
+					/>
+					<DetailItem
+						label="Métier"
+						value={patient.occupation || "Non renseigné"}
+					/>
+					<DetailItem
+						label="Latéralité"
+						value={
+							handednessTranslations[patient.handedness] ||
+							"Non renseignée"
+						}
+					/>
+					<DetailItem
+						label="Activité physique"
+						value={patient.physicalActivity || "Non renseignée"}
+					/>
+				</div>
+			</SectionToggle>
+
+			{/* Antécédents médicaux */}
+			<SectionToggle
+				title="Antécédents médicaux"
+				isOpen={openSections.medicalHistory}
+				onToggle={() => toggleSection("medicalHistory")}
+			>
+				<DetailItem
+					label="Antécédents chirurgicaux"
+					value={patient.surgicalHistory || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Antécédents traumatiques"
+					value={patient.traumaHistory || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Antécédents rhumatologiques"
+					value={patient.rheumatologicalHistory || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Correction visuelle ?"
+					value={
+						yesNoTranslations[patient.hasVisionCorrection] ||
+						"Non renseignée"
+					}
+				/>
+				<DetailItem
+					label="Nom de l'ophtalmologiste"
+					value={patient.ophtalmologistName || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Problèmes ORL"
+					value={patient.entProblems || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Nom du médecin ORL"
+					value={patient.entDoctorName || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Problèmes digestifs"
+					value={patient.digestiveProblems || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Nom du médecin digestif"
+					value={patient.digestiveDoctorName || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Informations HDLM"
+					value={patient.hdlm || "Non renseignées"}
+				/>
+				<DetailItem
+					label="Traitements en cours"
+					value={patient.currentTreatment || "Non renseigné"}
+				/>
+			</SectionToggle>
+
+			{/* Informations sur la famille */}
+			<SectionToggle
+				title="Informations sur la famille"
+				isOpen={openSections.familyInfo}
+				onToggle={() => toggleSection("familyInfo")}
+			>
+				<DetailItem
+					label="Fumeur ?"
+					value={
+						yesNoTranslations[patient.isSmoker] || "Non renseigné"
+					}
+				/>
+				<DetailItem
+					label="Contraception"
+					value={
+						contraceptionTranslations[patient.contraception] ||
+						"Non renseignée"
+					}
+				/>
+				<DetailItem
+					label="Enfants"
+					value={patient.hasChildren === "true" ? "Oui" : "Non"}
+				/>
+				{patient.childrenAges && (
+					<div>
+						<h4 className="text-md font-semibold m-2 mt-4">
+							Âges des enfants
+						</h4>
+						{patient.childrenAges.length > 0 ? (
+							patient.childrenAges.map((age, index) => (
+								<DetailItem
+									key={index}
+									label={`Enfant ${index + 1}`}
+									value={`${age} ans`}
+								/>
+							))
+						) : (
+							<p>Pas d'enfants renseignés</p>
+						)}
 					</div>
 				)}
+			</SectionToggle>
+
+			{/* Informations du praticien et cabinet */}
+			<SectionToggle
+				title="Informations du praticien et cabinet"
+				isOpen={openSections.practitionerInfo}
+				onToggle={() => toggleSection("practitionerInfo")}
+			>
+				<DetailItem
+					label="Médecin traitant"
+					value={patient.generalPractitioner || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Ostéopathe"
+					value={patient.osteopath?.name || "Non renseigné"}
+				/>
+				<DetailItem
+					label="Cabinet"
+					value={patient.cabinet?.name || "Non renseigné"}
+				/>
+			</SectionToggle>
+
+			{/* Documents médicaux et consultations */}
+			<div className="mb-4">
+				<h3 className="text-lg font-semibold">
+					Documents et consultations
+				</h3>
+				<DetailItem
+					label="Documents médicaux"
+					value={`${
+						patient.medicalDocuments?.length || 0
+					} document(s)`}
+				/>
+				<DetailItem
+					label="Consultations"
+					value={`${
+						patient.consultations?.length || 0
+					} consultation(s)`}
+				/>
+				<DetailItem
+					label="Rendez-vous"
+					value={`${patient.appointments?.length || 0} rendez-vous`}
+				/>
 			</div>
 
-			{/* Medical Problems Section */}
+			{/* Dates de création et de mise à jour */}
 			<div className="mb-4">
-				<button
-					className="flex justify-between items-center w-full bg-gray-700 p-2 rounded-md hover:bg-gray-600 text-gray-200"
-					onClick={() => toggleSection("medicalProblems")}
-				>
-					<span className="font-semibold text-sm md:text-base">
-						Antécédents médicaux
-					</span>
-					<span>{openSections.medicalProblems ? "−" : "+"}</span>
-				</button>
-				{openSections.medicalProblems && (
-					<div className="p-4">
-						<p>
-							<strong>Antécédents chirurgicaux:</strong>{" "}
-							{patient.surgicalHistory || "Non renseigné"}
-						</p>
-						<p>
-							<strong>Problèmes digestifs:</strong>{" "}
-							{patient.digestiveProblems || "Non renseigné"}
-						</p>
-						<p>
-							<strong>Nom du médecin gastro-entérologue:</strong>{" "}
-							{patient.digestiveDoctorName || "Non renseigné"}
-						</p>
-						<p>
-							<strong>Problèmes ORL:</strong>{" "}
-							{patient.entProblems || "Non renseigné"}
-						</p>
-						<p>
-							<strong>Nom du médecin ORL:</strong>{" "}
-							{patient.entDoctorName || "Non renseigné"}
-						</p>
-						<p>
-							<strong>Antécédents traumatiques:</strong>{" "}
-							{patient.traumaHistory || "Non renseigné"}
-						</p>
-						<p>
-							<strong>Antécédents rhumatologiques:</strong>{" "}
-							{patient.rheumatologicalHistory || "Non renseigné"}
-						</p>
-					</div>
-				)}
-			</div>
-
-			{/* Children Section */}
-			<div className="mb-4">
-				<button
-					className="flex justify-between items-center w-full bg-gray-700 p-2 rounded-md hover:bg-gray-600 text-gray-200"
-					onClick={() => toggleSection("children")}
-				>
-					<span className="font-semibold text-sm md:text-base">
-						Enfants
-					</span>
-					<span>{openSections.children ? "−" : "+"}</span>
-				</button>
-				{openSections.children && (
-					<div className="p-4">
-						<p>
-							<strong>Le patient a des enfants ?</strong>{" "}
-							{yesNoTranslations[patient.hasChildren] ||
-								"Non renseigné"}
-						</p>
-						{patient.hasChildren &&
-						patient.childrenAges &&
-						patient.childrenAges.length > 0 ? (
-							<p>
-								<strong>Âges des enfants:</strong>{" "}
-								{patient.childrenAges
-									.map(
-										(age) =>
-											`${age} ${age === 1 ? "an" : "ans"}`
-									)
-									.join(", ")}
-							</p>
-						) : null}
-					</div>
-				)}
+				<DetailItem
+					label="Créé le"
+					value={new Date(patient.createdAt).toLocaleDateString(
+						"fr-FR"
+					)}
+				/>
+				<DetailItem
+					label="Mis à jour le"
+					value={new Date(patient.updatedAt).toLocaleDateString(
+						"fr-FR"
+					)}
+				/>
 			</div>
 		</div>
 	);
