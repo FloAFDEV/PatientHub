6f65f926bb37a13cb18cb141473108eaf08b5b0d
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2024-11-17 15:48:22 +0100

    "Update Footer component: changed footer background color and added space to paragraph class"

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
 	return (
-		<footer className="mt-auto py-4">
-			<p className="text-secondary text-sm md:text-base font-light text-center">
+		<footer className="mt-auto py-4 bg-gray-200 dark:bg-gray-900">
+			<p className="text-secondary text-sm md:text-base font-light text-center ">
 				© 2024 AFDEV. Tous droits réservés.
 			</p>
 		</footer>
