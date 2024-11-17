fade1396deafaa938ffdc8e45dc7621f2666b17d
FloAFDEV <103335500+FloAFDEV@users.noreply.github.com>
2024-11-17 16:12:18 +0100

    Remove NodeJS namespace and simplify global prisma declaration.

--- components/lib/connect.ts
+++ components/lib/connect.ts
@@ -1,15 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-	namespace NodeJS {
-		interface Global {
-			prisma: PrismaClient | undefined;
-		}
-	}
-}
-
-declare global {
-	// eslint-disable-next-line no-var
 	var prisma: PrismaClient | undefined;
 }
 
